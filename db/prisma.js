08e2f2e41fd049c5ca3c18ec77cf0af2237d717a
John R. McGarvey <johnmcgarvey55@gmail.com>
2025-10-19 19:29:28 -0400

    fix so process exits if the database is not accessible

--- db/prisma.js
+++ db/prisma.js
@@ -1,4 +1,16 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+async function init() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to DB");
+  } catch (e) {
+    console.error("Failed to connect to DB", e);
+    process.exit(1);
+  }
+}
+
+init();
+
 module.exports = prisma;
