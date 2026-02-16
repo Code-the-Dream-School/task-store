e13808bcaf9039f4295bde47be5bfb721308f796
John McGarvey <johnmcgarvey55@gmail.com>
2026-02-16 17:14:59 -0500

    require CSRF token for read operations on protected routes

--- middleware/jwtMiddleware.js
+++ middleware/jwtMiddleware.js
@@ -17,17 +17,10 @@ module.exports = async (req, res, next) => {
         send401(res);
       } else {
         req.user = { id: decoded.id };
-        if (
-          ["POST", "PATCH", "PUT", "DELETE", "CONNECT"].includes(req.method)
-        ) {
-          // for these operations we have to check for cross site request forgery
-          if (req.get("X-CSRF-TOKEN") == decoded.csrfToken) {
-            next();
-          } else {
-            send401(res);
-          }
-        } else {
+        if (req.get("X-CSRF-TOKEN") == decoded.csrfToken) {
           next();
+        } else {
+          send401(res);
         }
       }
     });
